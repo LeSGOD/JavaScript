c503ffc6ee47e1d8fbc09734254084ab4bf24891
LeSGOD <artles00@wp.pl>
2020-06-21 22:08:52 +0200

    JSExercises

---
+++ JavaScript_Level_One/Part3.js
@@ -0,0 +1,7 @@
+//What is the weight in pounds (lbs)?
+//That is:  9.08 kilograms
+//Conversion Completed
+var weight = prompt("What is the weight in pounds(lbs?)")
+var weight = weight * 0.454
+alert("That is: " + weight + "kilograms")
+console.log("Conversion Completed");
